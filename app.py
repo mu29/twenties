9dfc7cdf02dca995210c4b3bdc5038d147a4acff
InJung Chung <mu29gl@gmail.com>
2015-12-06 17:12:46 +0900

    설정 수정

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 #-*- coding: utf-8 -*-
 
 from flask import Flask
-from settings import MODULES
+from flask.ext.sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
+db = SQLAlchemy(app)
 
 # 모듈 불러오기
-for module in MODULES:
-    __import__(module)
+# for module in MODULES:
+#    __import__(module)
 
 @app.route('/')
 def index():
