042a93cb398d818ef3d0ceb186a4f39e93d63430
InJung Chung <mu29gl@gmail.com>
2015-12-08 00:56:38 +0900

    MySQL 설정 넣고 postgresql 빼자

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 #-*- coding: utf-8 -*-
 
 from flask import *
-from flask.ext.sqlalchemy import SQLAlchemy
+#from flask.ext.sqlalchemy import SQLAlchemy
 from load_modules import MODULES
 import os
 
 app = Flask(__name__)
-app.config.from_object(os.environ['APP_SETTINGS'])
-db = SQLAlchemy(app)
+
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'projectDanbi'
+app.config['MYSQL_DATABASE_DB'] = 'twenties'
+
+#app.config.from_object(os.environ['APP_SETTINGS'])
+#db = SQLAlchemy(app)
 
 def init():
     for module in MODULES:
