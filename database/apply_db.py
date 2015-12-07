1562c8ba74964923f0909327795b5cc429cfb2c2
InJung Chung <mu29gl@gmail.com>
2015-12-08 01:50:11 +0900

    데이터 삽입 오류 수정

--- database/apply_db.py
+++ database/apply_db.py
@@ -11,20 +11,20 @@ class ApplyDB:
         self.connection = db.get_mysql().connect()
         self.connection.autocommit(True)
 
-    def put(self, names, phones, gender):
+    def put(self, names, phones, university, gender):
         if not self.validate(phones):
             return '실패!'
 
         cursor = self.connection.cursor(MySQLdb.cursors.DictCursor)
         cursor.execute("SELECT count(DISTINCT `group`) AS group_id FROM `apply`;")
 
-        query = "INSERT INTO `apply` SET `group`='{0}', `name`='{1}', `phone`='{2}', `gender`='{3}';"
+        query = "INSERT INTO `apply` SET `group`='{0}', `name`='{1}', `phone`='{2}', `university`='{3}', `gender`='{4}';"
         group_id = cursor.fetchone()['group_id'] + 1
 
         cursor.close()
         for i in range(0, len(names)):
             cursor = self.connection.cursor(MySQLdb.cursors.DictCursor)
-            cursor.execute(query.format(group_id, names[i], phones[i], gender))
+            cursor.execute(query.format(group_id, names[i], phones[i], university, gender))
             cursor.close()
 
         return None
@@ -33,7 +33,7 @@ class ApplyDB:
         cursor = self.connection.cursor(MySQLdb.cursors.DictCursor)
         query = "SELECT * FROM `apply` WHERE `phone` IN ("
         for i in range(0, len(phones)):
-            query = query + phones[i] + ", "
+            query = query + "'" + phones[i] + "', "
         query = query[:len(query) - 2] + ");"
         cursor.execute(query)
 
