1448dea8259009f3374bd5ea0ae3585c13e8a834
InJung Chung <mu29gl@gmail.com>
2016-03-20 16:23:02 +0900

    상대방 고르기!

---
+++ pages/select.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from flask import *
+from app import *
+
+@app.route('/select', methods=['POST', 'GET'])
+def select():
+    if request.method == 'POST':
+    else:
+        
